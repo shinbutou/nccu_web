457c9e168d6716af0fdb6a083fd08c544f2b1520
YuShan <c54513.candy@gmail.com>
2020-06-08 22:50:25 +0800

    Change FORM from POST to GET. Can write data into json now.

--- main.py
+++ main.py
@@ -10,11 +10,35 @@ def main():
    db_array = json.load(db_file)
    return render_template("main.html", data=db_array)
 
+
 @app.route("/main.html", methods=["POST", "GET"])
-def report():
-   print(db_array[0])
-def manage():
-   print(db_array[1])
+def rsubmit():
+    if request.method == "GET":
+
+        Con = request.values['con']     
+        Des = request.values['des']
+        Loc = request.values['loc']
+        Lat = request.values['lat']
+        Lon = request.values['lon']
+
+        newData = {
+            "Condition":Con,
+            "Description":Des,
+            "Location":Loc,
+            "lat":Lat,
+            "lon":Lon
+        }
+        
+        read_file =  open("./schema/Data.json","r",encoding="utf-8")
+        json_array = json.load(read_file)
+         
+        json_array.append(newData)
+        
+        input_file =  open("./schema/Data.json","w",encoding="utf-8")
+        json.dump(json_array,input_file,ensure_ascii=False) 
+
+        return render_template("main.html", data = json_array)    
+        
 
 if __name__ == "__main__":
     app.run()
