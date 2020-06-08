269f1ae81766d323ebd5084856a9dc0be6f0238f
YuShan <c54513.candy@gmail.com>
2020-06-09 00:21:42 +0800

    Separate data2.json

--- main.py
+++ main.py
@@ -8,25 +8,26 @@ app = Flask(__name__)
 def main():
    db_file =  open("./schema/Data.json", "r", encoding="utf-8")
    db_array = json.load(db_file)
-   return render_template("main.html", data=db_array)
-
+   db2_file =  open("./schema/data2.json", "r", encoding="utf-8")
+   db2_array = json.load(db2_file)
+   return render_template("main.html", data = db_array , data2 = db2_array  )
 
 @app.route("/main.html", methods=["POST", "GET"])
 def rsubmit():
     if request.method == "GET":
 
-        Con = request.values['con']     
-        Des = request.values['des']
-        Loc = request.values['loc']
-        Lat = request.values['lat']
-        Lon = request.values['lon']
+        con = request.values['con']     
+        des = request.values['des']
+        loc = request.values['loc']
+        lat = request.values['lat']
+        lon = request.values['lon']
 
         newData = {
-            "Condition":Con,
-            "Description":Des,
-            "Location":Loc,
-            "lat":Lat,
-            "lon":Lon
+            "Condition":con,
+            "Description":des,
+            "Location":loc,
+            "lat":lat,
+            "lon":lon
         }
         
         read_file =  open("./schema/Data.json","r",encoding="utf-8")
@@ -36,9 +37,12 @@ def rsubmit():
         
         input_file =  open("./schema/Data.json","w",encoding="utf-8")
         json.dump(json_array,input_file,ensure_ascii=False) 
+   
+        db2_file =  open("./schema/data2.json", "r", encoding="utf-8")
+        db2_array = json.load(db2_file)
 
-        return render_template("main.html", data = json_array)    
-        
+        return render_template("main.html", data = json_array, data2 = db2_array)    
+ 
 
 if __name__ == "__main__":
     app.run()
