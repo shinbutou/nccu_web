29d276faa2dc1779c8745ade78e6b17accf6c767
mozium <ChengHsi.Yang@gmail.com>
2020-06-09 02:03:02 +0800

    successfully added the browsing function

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
    db_array = json.load(db_file)
    db2_file =  open("./schema/data2.json", "r", encoding="utf-8")
    db2_array = json.load(db2_file)
-   return render_template("main.html", data = db_array , data2 = db2_array  )
+   return render_template("main.html", data = db_array , data2 = db2_array)
 
 @app.route("/main.html", methods=["POST", "GET"])
 def rsubmit():
@@ -30,18 +30,18 @@ def rsubmit():
             "lon":lon
         }
         
-        read_file =  open("./schema/Data.json","r",encoding="utf-8")
+        read_file =  open("./schema/Data.json", "r",encoding="utf-8")
         json_array = json.load(read_file)
          
         json_array.append(newData)
         
-        input_file =  open("./schema/Data.json","w",encoding="utf-8")
+        input_file =  open("./schema/Data.json", "w",encoding="utf-8")
         json.dump(json_array,input_file,ensure_ascii=False) 
    
         db2_file =  open("./schema/data2.json", "r", encoding="utf-8")
         db2_array = json.load(db2_file)
 
-        return render_template("main.html", data = json_array, data2 = db2_array)    
+        return render_template("main.html", data = json_array, data2 = db2_array)
  
 
 if __name__ == "__main__":
