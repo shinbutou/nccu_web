df4ee3b9d2f12f50b3eec7f18e3bd0de10b0db4c
YuShan <c54513.candy@gmail.com>
2020-06-09 05:22:33 +0800

    Change Chart, Change Data

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, redirect, url_for, render_template, request
 import json
 import pandas as pd
+import datetime
+import random
 
 app = Flask(__name__)
 
@@ -18,16 +20,26 @@ def rsubmit():
 
         con = request.values['con']     
         des = request.values['des']
-        loc = request.values['loc']
+        dis = request.values['dis']
         lat = request.values['lat']
         lon = request.values['lon']
+        x = datetime.datetime.now() #現在時間
+        mth = x.month
+        #dat = x.day     
+        dat = random.randint(1,9)       
+        hr = x.hour
+        sv = random.randint(0,1) 
 
         newData = {
             "Condition":con,
             "Description":des,
-            "Location":loc,
+            "dis":dis,
             "lat":lat,
-            "lon":lon
+            "lon":lon, 
+            "mth":mth,
+            "dat":dat,
+            "hr":hr,
+            "sv":sv
         }
         
         read_file =  open("./schema/Data.json", "r",encoding="utf-8")
